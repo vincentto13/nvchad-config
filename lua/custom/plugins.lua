9297e7606a3e98233957d884c249ba76a1affffb
Paweł Rapkiewicz <pawel.rapkiewicz@gmail.com>
2025-04-18 07:17:17 +0000

    custom config

---
+++ lua/custom/plugins.lua
@@ -0,0 +1,30 @@
+local plugins = {
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    event = "VeryLazy",
+    opts = function()
+      return require "custom.configs.null-ls"
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end,
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "clangd",
+        "clang-format",
+        "pyright",
+        "mypy",
+        "ruff",
+        "black",
+      }
+    }
+  }
+}
+return plugins
